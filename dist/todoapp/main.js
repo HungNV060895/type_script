f446be49dee724c088057b4ff18b7d9802c8aaac
HungNV <hung@m.ibis.studio>
2026-04-21 08:49:31 +0700

    update todo app

--- dist/todoapp/main.js
+++ dist/todoapp/main.js
@@ -9,19 +9,32 @@ const tableTodo = document.getElementById('totoTable');
 const handleResetInput = () => {
     txtTodo.value = '';
 };
-btnAdd.addEventListener('click', () => {
+btnAdd?.addEventListener('click', () => {
     const todoID = getRandomInt(1, 100);
-    const value = txtTodo.value;
-    handleResetInput();
-    const todo = {
-        id: todoID,
-        todoName: value,
-        completed: false
-    };
-    //JSON.stringify(todo)
-    const todoArr = JSON.parse(localStorage.getItem('todos') || '[]');
-    localStorage.setItem('todos', JSON.stringify([...todoArr, todo]));
-    window.location.reload();
+    if (txtTodo) {
+        const value = txtTodo.value;
+        handleResetInput();
+        const todo = {
+            id: todoID,
+            todoName: value,
+            completed: false
+        };
+        //Cách 1
+        //JSON.stringify(todo)
+        // const todoArr: Todo[] = JSON.parse(localStorage.getItem('todos') || '[]');
+        // localStorage.setItem('todos', JSON.stringify([...todoArr, todo]));
+        //Cách 2
+        const todoStr = localStorage.getItem('todos');
+        if (todoStr) {
+            const arrTodo = JSON.parse(todoStr);
+            arrTodo.push(todo);
+            localStorage.setItem('todos', JSON.stringify(arrTodo));
+        }
+        else {
+            localStorage.setItem('todos', JSON.stringify([todo]));
+        }
+        window.location.reload();
+    }
 });
 const todoData = JSON.parse(localStorage.getItem('todos') || '[]');
 todoData.forEach((item) => {
@@ -29,6 +42,16 @@ todoData.forEach((item) => {
             <td>${item.id}</td>
             <td>${item.todoName}</td>
             <td>${item.completed ? 'Yes' : 'No'}</td>
+            <td><button type="button" class="btn btn-danger btn-delete" data-todo="${item.id}">Delete</button></td>
         </tr>`);
+});
+const btnDelete = document.querySelectorAll('.btn-delete');
+btnDelete.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const id = btn.getAttribute('data-todo');
+        const newTodoData = todoData.filter((item) => item.id !== parseInt(id));
+        localStorage.setItem('todos', JSON.stringify(newTodoData));
+        location.reload();
+    });
 });
 export {};
--- src/todoapp/main.ts
+++ src/todoapp/main.ts
@@ -1,4 +1,3 @@
-export { }
 
 type Todo = {
     id: number,
@@ -23,25 +22,44 @@ const handleResetInput = () => {
     txtTodo.value = '';
 }
 
-btnAdd.addEventListener('click', () => {
+
+btnAdd?.addEventListener('click', () => {
     const todoID = getRandomInt(1, 100);
-    const value = txtTodo.value;
+    if(txtTodo){
+        const value = txtTodo.value;
+        handleResetInput();
 
-    handleResetInput();
+        const todo: Todo = {
+            id: todoID,
+            todoName: value,
+            completed: false
+        }
 
-    const todo: Todo = {
-        id: todoID,
-        todoName: value,
-        completed: false
+
+        //Cách 1
+        //JSON.stringify(todo)
+        // const todoArr: Todo[] = JSON.parse(localStorage.getItem('todos') || '[]');
+
+        // localStorage.setItem('todos', JSON.stringify([...todoArr, todo]));
+
+
+
+        //Cách 2
+        const todoStr = localStorage.getItem('todos');
+
+        if(todoStr){
+            const arrTodo = JSON.parse(todoStr);
+            arrTodo.push(todo);
+            localStorage.setItem('todos', JSON.stringify(arrTodo));
+        }else{
+            localStorage.setItem('todos', JSON.stringify([todo]));
+        }
+
+        window.location.reload();
     }
-
-    //JSON.stringify(todo)
-    const todoArr: Todo[] = JSON.parse(localStorage.getItem('todos') || '[]');
-
-    localStorage.setItem('todos', JSON.stringify([...todoArr, todo]));
-
-    window.location.reload();
 })
+
+
 
 
 const todoData = JSON.parse(localStorage.getItem('todos') || '[]');
@@ -52,6 +70,20 @@ todoData.forEach((item: Todo) => {
             <td>${item.id}</td>
             <td>${item.todoName}</td>
             <td>${item.completed ? 'Yes' : 'No'}</td>
+            <td><button type="button" class="btn btn-danger btn-delete" data-todo="${item.id}">Delete</button></td>
         </tr>`
     )
 });
+
+const btnDelete = document.querySelectorAll('.btn-delete');
+
+btnDelete.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const id = btn.getAttribute('data-todo') as string;
+        const newTodoData = todoData.filter((item: Todo) => item.id !== parseInt(id));
+        localStorage.setItem('todos', JSON.stringify(newTodoData));
+        location.reload();
+    })
+})
+
+export { }
